42543909cd6f0768a62aa354426795abcb65ffd3
Tom McEvoy <tjmcevoy@yahoo.com>
2015-01-10 23:17:06 -0500

    try again

---
+++ plot1.R
@@ -0,0 +1,39 @@
+## Plot 1
+## download dataset
+
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file <-"PowerConsumption"
+download.file(url,file)
+
+## unzip, import data into file
+
+unzip(file)
+powerdata <-read.csv("household_power_consumption.txt",sep=";")
+
+## change columns 1 and 2 from "factor" to "char"
+
+# change columns 3 to 9 from "factor" to "numeric"
+
+## convert date from Factor to Date
+
+powerdata$Date <- as.Date(powerdata$Date, format="%d/%m/%Y")
+
+## extract the time period needed 
+## (2007-02-01 to 2007-02-02)
+
+extract <- subset(powerdata, subset=(Date >="2007-02-01" & Date <="2007-02-02"))
+
+## create histogram plot
+
+extract$Global_active_power<-as.numeric(extract$Global_active_power)
+hist(extract$Global_active_power,main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
+
+## copy to PNG file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
+     
+     
+     
+
+     
+     
